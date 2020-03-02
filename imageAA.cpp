5afc313a22e9afe664dcd7c2d0b37132f9426811
John Mamutuk <colonel.zack@yahoo.com>
2020-03-03 02:56:58 +0800

    Updated functions in imageAA, including significant changes to structure

--- imageAA.cpp
+++ imageAA.cpp
@@ -2,146 +2,203 @@
 #include <wx/image.h>
 #include <vector>
 
-class imageAA{
-	
-	public wxImage uploadImage(wxString imageName)
-	{
-		wxImage uploadedImage;
-	
-		while (!uploadedImage.IsOk())
-		{
-//			uploadedImage.LoadFile	( 	const wxString & name, 
-//										const wxString & mimetype,
-//										int  index = -1 
-//									);
-		}
-		
-		return uploadedImage
-	}
-	
-//---------------------------------------------------------------------
-	
-	private std::vector< vector< vector< double > > > 
-							retrieveImageColor (wxImage image)
-	{
-		int width, height;
-		double red, green, blue, luma;
-		std::vector< vector< vector< unsigned char > > > color;
-		
-		height = image.GetHeight();
-		width = image.GetWidth();
-		
-		for (int y = 0; y < height; y++)
-		{
-			retrieveColor.pushback();
-			
-			for (int x = 0; x < width; x++)
-			{
-				red = image.GetRed(x, y) / 255;
-				green = image.GetGreen(x, y) / 255;
-				blue = image.GetBlue(x, y) / 255;
-				luma = colorToLuma(red, green, blue)
-				
-				color[y].push_back({red, green, blue, luma});
-			}
-		}
-		
-		return color;
-	}
-	
-//	private std::vector< vector< unsigned char > > convertColortoLuminance
-//										(std::vector< vector< vector < unsigned char > > > color)
-//	{
-//		std::vector< vector<unsigned char> > luminance;
-//		unsigned char currLuminance;
-//		
-//		height = color.size();
-//		width = color[0].size();
-//		
-//		for (int y = 0; y < height; y++)
-//		{
-//			luminance.pushback();
-//			
-//			for (int x = 0; x < width; x++)
-//			{
-//				currLuminance = color[y][x][0]*0.2126 + color[y][x][1]*0.7152 + color[y][x][2]*0.0722;
-//				luminance.pushback[y]](currLuminance);
-//			}
-//		}
-//	}
-	
-	private unsigned char colorToLuma	(	double red, 
-											double green, 
-											double blue
-										)
-	{
-		double luma;
-		luma = red*0.2126 + green*0.7152 + blue*0.0722;
-		return luma;
-	}
-	
-	
-	private std::vector< vector< bool > > detectEdge 
-		(	std::vector< vector< vector< unsigned char > > > color,
-			float edgeThreshold, 
-			float edgeThresholdMin
-		)
-	{
-		std::vector< vector< bool > > edgeDetected;
-		height = color.size();
-		width = color[0].size();
-		double luma[5]
-		double rangeMin, rangeMax, range;
-		
-		for (int y = 1; y < height-1; y++)
-		{
-			edgeDetected.pushback();
-			
-			for (int x = 1; x < width-1; x++)
-			{
-				luma[0] = color[y-1][x][3]; //Top Pixel
-				luma[1] = color[y][x-1][3]; //left Pixel
-				luma[2] = color[y][x][3]; //Current Pixel
-				luma[3] = color[y][x+1][3]; //Right Pixel
-				luma[4] = color[y+1][x][3]; //Bottom Pixel
-				
-				rangeMin = luma[0];
-				rangeMax = luma[0];
-				
-				for (int i = 1; i < 5; i++)
-				{
-					rangeMin = luma[i] < rangeMin ? luma[i] : rangeMin;
-					rangeMax = luma[i] > rangeMax ? luma[i] : rangeMax;
-				}
-				
-				range = rangeMax - rangeMin;				
-				
-//				float rangeMin = min(lumaM, min(min(lumaN, lumaW), min(lumaS, lumaE)));
-//				float rangeMax = max(lumaM, max(max(lumaN, lumaW), max(lumaS, lumaE)));
-//				float range = rangeMax -rangeMin;
-//				if(range < max(FXAA_EDGE_THRESHOLD_MIN, rangeMax * XAA_EDGE_THRESHOLD)) return FxaaFilterReturn(rgbM);
-				
-				if (range < edgeThresholdMin || range < edgeThreshold * rangeMax)
-				{
-					edgeDetected[y].pushback(false);
-				}
-				else
-				{
-					edgeDetected[y].pushback(true);
-				}
-			}
-		}
-		
-		return edgeDetected;
-	} 
-	
-	private wxImage antiAliasImage 	(	wxImage inputImage, 
-										float edgeThresholdMin, 
-										float edgeThreshold
-									)
-	{
-		std::vector< vector< vector< double > > > color = retrieveImageColor (inputImage);
-		std::vector< vector< bool > > edges = (color, edgeThreshold, edgeThresholdMin);
-	}
-};
+//-------------------------------------------------------------------
+//Math function recreations for doubles
+//-------------------------------------------------------------------
 
+private double dmax (double a, double b)
+{
+	double max;
+	max = b > a ? b : a;
+	return max;
+}
+
+private double dmin (double a, double b)
+{
+	double min;
+	min = b < a ? b : a; 
+	return min;
+}
+
+private double dabs (double a)
+{
+	double abs;
+	abs = a > 0 ? a : -a;
+	return abs;
+}
+
+//---------------------------------------------------------------------
+//Main program
+//---------------------------------------------------------------------
+
+//Store Image as 3D vector (width, height, RGB)
+	
+private std::vector< vector< vector< unsigned char > > > 
+						retrieveImageColor (wxImage image)
+{
+	std::vector< vector< vector< unsigned char > > > color;
+	int width, height;
+	unsigned char red, green, blue;
+	
+	height = image.GetHeight();
+	width = image.GetWidth();
+	
+	for (int y = 0; y < height; y++)
+	{
+		retrieveColor.pushback();
+		
+		for (int x = 0; x < width; x++)
+		{
+			red = image.GetRed(x, y);
+			green = image.GetGreen(x, y);
+			blue = image.GetBlue(x, y);
+			
+			color[y].push_back({red, green, blue});
+		}
+	}
+	
+	return color;
+}
+
+//Convert 3D vector of colors (width, height, RGB) to Image
+
+private wxImage outputImage (std::vector< vector< vector< unsigned char > > > color)
+{
+	int height, width;
+	unsigned char red, green, blue;
+	
+	height = color.size();
+	width = color[0].size();
+	
+	Image = wxImage(width, height);
+	
+	for (int y = 0; y < height; y++)
+	{
+		retrieveColor.pushback();
+		
+		for (int x = 0; x < width; x++)
+		{
+			red = color[y][x][0];
+			green = color[y][x][1];
+			blue = color[y][x][2];
+			
+			Image.SetRGB(x, y, red, green, blue);
+		}
+	}
+	
+	return Image;
+}
+
+//--------------------------------------------------------------------
+
+//Compute for the luma values of all pixels in the image
+
+private std::vector< vector< double > > convertColortoLuma
+			(std::vector< vector< vector < unsigned char > > > color)
+{
+	std::vector< vector<unsigned char> > luma;
+	unsigned char currLuma;
+	
+	height = color.size();
+	width = color[0].size();
+	
+	for (int y = 0; y < height; y++)
+	{
+		luma.pushback();
+		
+		for (int x = 0; x < width; x++)
+		{
+			currLuma = 	(double) color[y][x][0] / 255 * 0.2126 
+						+ color[y][x][1] /255 * 0.7152
+						+ color[y][x][2] /255 * 0.0722;
+			luma.pushback[y]](currLuma);
+		}
+	}
+	
+	return luma;
+}
+
+//Determine whether current edge pixel is Horizontal or Vertical
+
+private bool classifyEdgeHorizontal(std::vector< vector< double > > luma, int y, int x)
+{
+	bool isHorizontal;
+	double edgeHorizontal, edgeVertical;
+	
+	edgeHorizontal = 	dabs(-2.0 * luma[y][x-1] + luma[y-1][x-1] + luma[y+1][x-1])  
+						+ dabs(-2.0 * luma[y][x] + luma[y-1][x] + luma[y+1][x]) * 2.0    
+						+ dabs(-2.0 * luma[y][x+1] + luma[y-1][x+1] + luma[y+1][x+1]);
+	edgeVertical = 	dabs(-2.0 * luma[y-1][x] + luma[y-1][x-1] + luma[y-1][x+1]) 
+					+ dabs(-2.0 * luma[y][x] + luma[y][x-1] + luma[y][x+1]) * 2.0  
+					+ dabs(-2.0 * luma[y+1][x] + luma[y+1][x-1] + luma[y+1][x+1]);
+					
+	isHorizontal = edgeHorizontal > edgeVertical ? True : False;
+	return isHorizontal
+}
+
+//Main function for antialiasing logic, takes inputs and outputs of
+//images in 3D vector format (width, height, RGB)
+
+private std::vector< vector< vector< unsigned char > > > antiAlias 
+	(	std::vector< vector< vector< unsigned char > > > color,
+		double edgeThreshold, 
+		double edgeThresholdMin
+	)
+{
+	std::vector< vector< vector< unsigned char > > > aaColor;
+	std::vector< vector< double > > luma; 
+	double rangeMin, rangeMax, range;
+	double edgeHorizontal, edgeVertical;
+	int height, width;
+	bool isHorizontal;
+	
+	height = color.size();
+	width = color[0].size();
+	
+	luma = convertColortoLuma(color)
+	
+	for (int y = 1; y < height-1; y++)
+	{
+		aaColor.pushback();
+		
+		for (int x = 1; x < width-1; x++)
+		{
+			rangeMin = dmin(luma[y][x], dmin(dmin(luma[y-1][x], luma[y][x-1]), dmin(luma[y+1][x], luma[y][x+1])));
+			rangeMax = dmax(luma[y][x], dmax(dmax(luma[y-1][x], luma[y][x-1]), dmax(luma[y+1][x], luma[y][x+1])));
+			
+			range = rangeMax - rangeMin;				
+				
+			if (range < edgeThresholdMin || 
+				range < edgeThreshold * rangeMax)
+			{
+				aaColor[y].pushback(color[y][x]);
+			}
+			
+			// Is the local edge horizontal or vertical ?
+			isHorizontal = classifyEdgeHorizontal(luma, y, x);
+			
+			
+		}
+	}
+	
+	return aaColor;
+} 
+
+//-------------------------------------------------------------------
+
+//Invokable external function for antialiasing
+//Includes the conversions to and from 3D vector format and wxImages 
+
+public wxImage antiAliasImage (wxImage inputImage){
+	wxImage outImg;
+	std::vector< vector< vector< unsigned char > > > color;
+	std::vector< vector< vector< unsigned char > > > outColor;
+	
+	color = retrieveImageColor(image);
+	outColor = 	antiAlias 	(	std::vector< vector< vector< unsigned char > > > color,
+								double edgeThreshold, 
+								double edgeThresholdMin
+							);
+	outImg = outputImage(outColor);
+	return outImg;
+}
